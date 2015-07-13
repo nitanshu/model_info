47a22a293a24b53287f3fa52b6c0b5310dc76fc9
nitanshu verma <nitanshu@headerlabs.com>
2015-07-14 00:33:09 +0530

    removed HABTM join tables

--- app/controllers/model_info/application_controller.rb
+++ app/controllers/model_info/application_controller.rb
@@ -2,12 +2,14 @@ module ModelInfo
   class ApplicationController < ActionController::Base
     private
     def models_tab
-      array=[]
+      array=[], @model_array=[]
       Rails.application.eager_load!
-      ActiveRecord::Base.descendants.each do |x|
-        array.push(x) if x.table_exists?
-        array.delete(ActiveRecord::SchemaMigration)
-        @model_array=array
+      array=ActiveRecord::Base.descendants.collect{|x| x.to_s if x.table_exists?}.compact
+      array.each do |x|
+        if  x.split('::').last.split('_').first != "HABTM"
+          @model_array.push(x)
+        end
+        @model_array.delete('ActiveRecord::SchemaMigration')
       end
     end
   end
