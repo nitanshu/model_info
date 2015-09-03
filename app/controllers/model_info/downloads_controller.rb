35e77b7f7b0f71b89386c1c37cd72d9092fe2f01
nitanshu verma <nitanshu@headerlabs.com>
2015-09-03 16:29:20 +0530

    download of associated data is done with a fix of bug

--- app/controllers/model_info/downloads_controller.rb
+++ app/controllers/model_info/downloads_controller.rb
@@ -1,8 +1,7 @@
 require_dependency "model_info/application_controller"
-require 'csv'
 module ModelInfo
 	class DownloadsController < ApplicationController
-		before_action :fetch_model_data
+		before_filter :authenticate_request
 
 		def download_csv
 			csv_string = CSV.generate do |csv|
@@ -19,22 +18,32 @@ module ModelInfo
 
 		def download_json
 			send_data @model_data.to_json,
-			    :type => 'text/json; charset=iso-8859-1; header=present',
-					:disposition => "attachment; filename=#{params[:model_class]}-#{DateTime.now}.json"
+			          :type => 'text/json; charset=iso-8859-1; header=present',
+			          :disposition => "attachment; filename=#{params[:model_class]}-#{DateTime.now}.json"
 		end
 
 		def download_xml
 			send_data @model_data.to_xml,
 			          :type => 'text/xml; charset=iso-8859-1; header=present',
-			          :disposition => "attachment; filename=#{params[:model_class]}-#{DateTime.now}.xml"
-		end
-
-		def download_excel
+			          :disposiftion => "attachment; filename=#{params[:model_class]}-#{DateTime.now}.xml"
 		end
 
 		private
+
+		def authenticate_request
+			if params[:associated_model]
+				fetch_associated_model_data
+			else
+				fetch_model_data
+			end
+		end
+
 		def fetch_model_data
 			@model_data = params[:model_class].constantize.all
+		end
+
+		def fetch_associated_model_data
+			@model_data = params[:model_class].constantize.find(params[:model_object_id]).send(params[:associated_model])
 		end
 	end
 end
--- app/controllers/model_info/models_controller.rb
+++ app/controllers/model_info/models_controller.rb
@@ -4,49 +4,53 @@ module ModelInfo
   class ModelsController < ApplicationController
     before_action :models_tab
     before_action :fetch_model_name, only: [:create, :update]
-
+    
     def index
       redirect_to model_display_url(model_class: @model_array.first)
     end
-
+    
     def display
       @model_class, @page = params[:model_class],params[:page]
       @model_pagination = @model_class.constantize.page(@page).per(10)
     end
-
+    
     def new
       @model_class = params[:model_class].constantize
       @model_data = @model_class.new
     end
-
+    
     def create
       @model_class=params[:model_class].constantize
       @model_class.create(permit_params)
-      redirect_to model_show_path(model_class: @model_class, model_object_id: @model_class.last.id)
+      if @model_class.last
+        redirect_to model_show_path(model_class: @model_class, model_object_id: @model_class.last.id)
+      else
+        redirect_to :back
+      end
     end
-
+    
     def edit
       @model_class=params[:model_class].constantize
       @model_data=@model_class.find(params[:model_object_id])
     end
-
+    
     def show
       @model_class=params[:model_class].constantize
       @model_data=@model_class.find(params[:model_object_id])
     end
-
+    
     def update
       params[:model_class].constantize.find(params[@model_name][:id]).update(permit_params)
       redirect_to model_show_path(model_class: params[:model_class].constantize, model_object_id: params[@model_name][:id])
     end
-
+    
     def destroy
       params[:model_class].constantize.find(params[:model_object_id]).destroy
       redirect_to :back
     end
-
+    
     private
-
+    
     def permit_params
       params.require(@model_name).permit!
     end
